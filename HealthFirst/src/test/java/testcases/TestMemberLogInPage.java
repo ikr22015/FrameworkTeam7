28f54fc4d0eeecebd87ef68d23e7f83a62adcf5f
Imran Kabir <ikr22015@gmail.com>
2018-01-03 02:19:41 -0500

    completed member login page

---
+++ HealthFirst/src/test/java/testcases/TestMemberLogInPage.java
@@ -0,0 +1,30 @@
+package testcases;
+
+import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import pageobject.HomePage;
+import pageobject.MemberLogInPage;
+
+public class TestMemberLogInPage extends CommonAPI {
+
+    @Test
+    public void testMemberLogIn(){
+        HomePage hp = PageFactory.initElements(driver,HomePage.class);
+        hp.goToMemberLogin();
+
+        MemberLogInPage mlp = PageFactory.initElements(driver, MemberLogInPage.class);
+        mlp.memberLogin();
+    }
+
+    @Test
+    public void testForgetPassword(){
+        HomePage hp = PageFactory.initElements(driver,HomePage.class);
+        hp.goToMemberLogin();
+
+        MemberLogInPage mlp = PageFactory.initElements(driver, MemberLogInPage.class);
+        mlp.memberLogin();
+
+        mlp.forgetPassword();
+    }
+}

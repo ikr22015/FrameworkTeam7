dfc8d703eb3aa0a87f1f937d999ad833fdae7edc
Imran Kabir <ikr22015@gmail.com>
2018-01-03 19:06:29 -0500

    completed health first member sign up

--- HealthFirst/src/test/java/testcases/TestMemberLogInPage.java
+++ HealthFirst/src/test/java/testcases/TestMemberLogInPage.java
@@ -27,4 +27,6 @@ public class TestMemberLogInPage extends CommonAPI {
 
         mlp.forgetPassword();
     }
+
+
 }
---
+++ HealthFirst/src/test/java/testcases/TestMemberSignUp.java
@@ -0,0 +1,24 @@
+package testcases;
+
+import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import pageobject.HomePage;
+import pageobject.MemberLogInPage;
+import pageobject.MemberSignUpPage;
+
+public class TestMemberSignUp extends CommonAPI{
+
+    @Test
+    public void testMemberSignUp(){
+        HomePage hp = PageFactory.initElements(driver,HomePage.class);
+        hp.goToMemberLogin();
+
+        MemberLogInPage mlp = PageFactory.initElements(driver, MemberLogInPage.class);
+        mlp.newMemberSignUp();
+
+        MemberSignUpPage msup = PageFactory.initElements(driver, MemberSignUpPage.class);
+        msup.clickAgree();
+        msup.fillUpForm();
+    }
+}
